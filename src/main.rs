6bb1432db3732b9923353e774c98f04b8cf884b0
crosstyan <crosstyan@outlook.com>
2022-08-23 07:45:12 +0000

    fix: use CString as parameter

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,23 @@ use agora_rtsa_rs::agoraRTC;
 use agora_rtsa_rs::agoraRTC::{LogLevel, VideoDataType, VideoFrameType, VideoStreamQuality};
 use agora_rtsa_rs::C::{self, video_data_type_e_VIDEO_DATA_TYPE_H264};
 use anyhow::{anyhow, bail, Context};
-use gst::glib;
-use gst::glib::closure;
 use gst::prelude::*;
 use gst::{element_error, Buffer, Memory};
 use gst_app::{AppSink, AppSrc};
-use log::{error, info, warn};
+use log::{error, info, warn, debug};
 use std::env;
 use std::ffi::{c_void, CString};
 use std::sync::{Arc, Mutex};
 use std::thread::sleep;
+
+trait ToCString {
+    fn to_c_string(&self) -> Result<CString, std::ffi::NulError>;
+}
+impl ToCString for &str {
+    fn to_c_string(&self) -> Result<CString, std::ffi::NulError> {
+        CString::new(&**self)
+    }
+}
 
 // Check if all GStreamer plugins we require are available
 fn check_plugins() -> Result<(), anyhow::Error> {
@@ -64,12 +71,10 @@ fn main() {
     let cert = std::fs::read_to_string(home.join("certificate.bin")).unwrap();
     let app_id = "3759fd9101e04094869e7e69b9b3fe64";
     let channel_name = "test";
-    let app_token = "007eJxTYLiz0kH/x3WBX3dO/GMTN3rG+cVYYduUmhwuppt8h85aPVRUYDA2N7VMS7E0NDBMNTAxsDSxMLNMNU81s0yyTDJOSzUz2X6EMdlMjDn5+foNLIwMEAjiszCUpBaXMDAAAGYbH8k=";
-    let app_token_c = CString::new(app_token).expect("CString::new error");
+    let app_token = "007eJxTYGj+/iiralfpa76AYn8j3nwV82MezTuifDn7ws3WCPY901FgMDY3tUxLsTQ0MEw1MDGwNLEws0w1TzWzTLJMMk5LNTMJaGFJvnCVNfntww2sjAwQCOKzMJSkFpcwMAAA6z8gJA==";
     let log_path = "logs";
-    let log_path_c = CString::new(log_path).unwrap();
     let uid: u32 = 1234;
-    let mut data = std::vec::Vec::<Buffer>::new();
+    // let mut data = std::vec::Vec::<Buffer>::new();
 
     // Set the default log level to debug
     if env::var("RUST_LOG").is_err() {
@@ -79,7 +84,6 @@ fn main() {
     let mut builder = env_logger::Builder::from_default_env();
     builder.target(env_logger::Target::Stdout);
     builder.init();
-    let mut data = std::vec::Vec::<Buffer>::new();
 
     gst::init();
     check_plugins();
@@ -91,7 +95,7 @@ fn main() {
     let log_cfg = agoraRTC::LogConfig {
         log_disable: false,
         log_disable_desensitize: true,
-        log_level: LogLevel::DEBUG,
+        log_level: LogLevel::INFO,
     };
 
     let opt = agoraRTC::RtcServiceOption {
@@ -102,7 +106,8 @@ fn main() {
     };
 
     // You have to set logs before deallocation
-    let opt_t: C::rtc_service_option_t = opt.to_c_type(log_path_c.as_ptr());
+    let log_path_c = log_path.to_c_string().unwrap();
+    let opt_t: C::rtc_service_option_t = opt.to_c_type(&log_path_c);
 
     /// If frame_per_sec equals zero, then real timestamp will be used. So I don't need to calculate them.
     let video_opt = C::video_frame_info_t {
@@ -126,16 +131,14 @@ fn main() {
     let pipeline = pipeline
         .downcast::<gst::Pipeline>()
         .expect("not a pipeline");
-    // let source = pipeline.by_name("src").expect("can't find src");
-    // Get access to the webrtcbin by name
+    // https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/blob/main/tutorials/src/bin/basic-tutorial-8.rs
     let appsink = pipeline
         .by_name("agora")
         .expect("can't find agora")
         .dynamic_cast::<AppSink>()
         .expect("should be an appsink");
-    // https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/blob/main/tutorials/src/bin/basic-tutorial-8.rs
 
-    result_verify(agoraRTC::init(app_id, opt_t, handlers), "init");
+    result_verify(agoraRTC::init(&app_id.to_c_string().unwrap(), opt_t, handlers), "init");
     let conn_id: u32 = match agoraRTC::create_connection() {
         Ok(id) => id,
         Err(code) => {
@@ -150,7 +153,8 @@ fn main() {
                 if let Ok(sample) = appsink.pull_sample() {
                     let buf = sample.buffer().unwrap().copy();
                     let mem = buf.all_memory().unwrap();
-                    // dbg!("{#?}", mem.clone());
+                    // dbg!(mem.clone());
+                    // dbg!(buf.clone());
                     // the buffer contains a media specific marker. for video this is the end of a frame boundary, for audio this is the start of a talkspurt.
                     // https://gstreamer.freedesktop.org/documentation/gstreamer/gstbuffer.html?gi-language=c#GstBufferFlags
                     let flags = buf.flags().contains(gst::BufferFlags::MARKER);
@@ -181,22 +185,30 @@ fn main() {
             })
             .build(),
     );
+
     let chan_opt = C::rtc_channel_options_t::new();
     result_verify(
         agoraRTC::join_channel(
             conn_id,
-            channel_name,
+            &channel_name.to_c_string().unwrap(),
             Some(uid),
-            app_token_c.as_ptr(),
+            &app_token.to_c_string().unwrap(),
             chan_opt,
         ),
         "join channel",
     );
+
     result_verify(agoraRTC::mute_local_audio(conn_id, true), "mute local audio");
+
+    // add some delay to make sure header sent
+    sleep(std::time::Duration::from_secs(2));
+
     pipeline
         .set_state(gst::State::Playing)
         .expect("set playing error");
+
     sleep(std::time::Duration::from_secs(60));
+
     pipeline
         .set_state(gst::State::Paused)
         .expect("set state error");
